06d10538d55a124c8ccba6ad37f27bbef4193315
Harichandana <harichandanavita@gmail.com>
2023-11-10 11:32:02 +0530

    router path creted to header,footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "header",
+    component: HeaderComponent
+  },
+  {
+    path: "section",
+    component: SectionComponent,
+  },
+  {
+    path: "footer",
+    component: FooterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
