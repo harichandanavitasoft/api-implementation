41356f7c839fa4a4a75aba6f831600c37a2c40ab
Harichandana <harichandanavita@gmail.com>
2023-11-10 12:51:08 +0530

    data printed

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,5 +6,8 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
   private geturl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-  constructor(privatehttp: HttpClient) { }
+  constructor(private http: HttpClient) { }
+  getData() {
+    return this.http.get(this.geturl);
+  }
 }
--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -7,8 +7,16 @@ import { ApiService } from '../api.service';
   styleUrls: ['./section.component.css']
 })
 export class SectionComponent implements OnInit {
+  apiInfo!: any;
   constructor(private apiservice: ApiService) { }
   ngOnInit(): void {
+    this.apiservice.getData().subscribe((res: any) => {
+      this.apiInfo = res;
+      console.log(this.apiInfo);
+
+
+
+    });
 
   }
 
